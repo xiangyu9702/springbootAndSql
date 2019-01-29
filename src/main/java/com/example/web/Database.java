ad10a232b5d690e296a88af0cafba24ab4c09197
xiangyu9702 <xiangyuchen9702@gmail.com>
2019-01-29 09:44:51 +0800

    UPDATE

--- src/main/java/com/example/web/Database.java
+++ src/main/java/com/example/web/Database.java
@@ -3,7 +3,7 @@ package com.example.web;
 public class Database {
     // JDBC 驱动名及数据库 URL
     public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/college?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=GMT&allowPublicKeyPRIMARYRetrieval=true";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/college?useSSL=false";
 
     // 数据库的用户名与密码，需要根据自己的设置
     public static final String USER = "root";
